d0d401ac25911aaf18552596c69dfe8bc8d2982f
zafran-ullah <zafranullah7@gmail.com>
2025-06-06 16:54:47 +0500

    CORS headers-3

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_talisman import Talisman
-from flask_cors import CORS
 
 app = Flask(__name__)
 talisman = Talisman(app)
-CORS(app)
+
+# export talisman so tests can import it
+__all__ = ['app', 'talisman']
