fa32941aa5565d98e5db26c3e2d0f624322d137e
zafran-ullah <zafranullah7@gmail.com>
2025-06-06 23:37:50 +0500

    add-docker

--- service/__init__.py
+++ service/__init__.py
@@ -8,9 +8,13 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
+from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
+talisman = Talisman(app)
+CORS(app)
 app.config.from_object(config)
 
 # Import the routes After the Flask app is created
@@ -24,7 +28,8 @@ from service.common import error_handlers, cli_commands  # noqa: F401 E402
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(
+    "  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
 
 try:
